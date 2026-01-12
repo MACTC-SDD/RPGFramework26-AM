123387e8e0968950ee9d6eb17aa42ae7a2cc1160
21001506CTC <21001506@student.mukegonisd.org>
2026-01-12 08:38:52 -0500

    refind equipment system

--- Items/EquipmentBase.cs
+++ Items/EquipmentBase.cs
@@ -34,15 +34,89 @@ public class EquipmentBase
             Item item = slots[slot];
 
             // If two-handed weapon, remove from both hands
-            if (item is Weapon weapon && weapon.IsTwoHanded)
+            if (item is not Weapon weapon || !weapon.IsTwoHanded)
+            {
+                slots.Remove(slot);
+            }
+            else
             {
                 slots.Remove(EquipmentSlot.MainHand);
                 slots.Remove(EquipmentSlot.OffHand);
             }
-            else
+        }
+        //the below is the code equipment weapon/Armor
+            public class Player
+        {
+            public Equipment Equipment { get; } = new Equipment();
+
+            public int Strength { get; private set; }
+            public int Defense { get; private set; }
+
+            public void RecalculateStats()
             {
-                slots.Remove(slot);
+                Strength = 0;
+                Defense = 0;
+
+                foreach (Item item in Equipment.GetEquippedItems())
+            {
+                    Strength += item.StrengthBonus;
+                    Defense += item.DefenseBonus;
+                }
+            }
+            }
+
+        private IEnumerable<Item> GetEquippedItems()
+        {
+            throw new NotImplementedException();
+        }
+    
+        public abstract class Item
+        {
+            public EquipmentSlot Slot { get; protected set; }
+
+            public int StrengthBonus { get; set; }
+            public int DefenseBonus { get; set; }
+            public object Name { get; internal set; }
+        }
+        public class Weapon : Item
+        {
+            public bool IsTwoHanded { get; set; }
+
+            public Weapon(bool twoHanded)
+            {
+                IsTwoHanded = twoHanded;
+                Slot = EquipmentSlot.MainHand;
             }
         }
+        public class Helmet : Item
+        {
+            public Helmet()
+            {
+                Slot = EquipmentSlot.Head;
+                DefenseBonus = 5;
+            }
+        }
+        public void EquipItem(Player player, Item item)
+        {
+            bool equipped = player.Equipment.Equip(item);
+
+            if (equipped)
+            {
+                player.RecalculateStats();
+                Console.WriteLine($"{item.Name} equipped!");
+            }
+        }
+
+        private bool Equip(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnequipHelmet(Player player)
+        {
+            player.Equipment.Unequip(EquipmentSlot.Head);
+            player.RecalculateStats();
+        }
+        
     }
 }
