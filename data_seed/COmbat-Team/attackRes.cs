dad156a64cf08aa8c738c715d6e403347c2a5710
61120045-dev <61120045@student.muskegonisd.org>
2026-01-13 09:43:05 -0500

    i worked on hit chance

---
+++ data_seed/COmbat-Team/attackRes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGFramework.data_seed.COmbat_Team
+{
+
+
+    public class hitChance
+    {
+
+
+
+        int HitChance = 50 + (Character.Dexterity -/*Enemy.Dexterity*/ ) * 5;
+        HitChance = Math.Clamp(HitChance,5,95)
+            int roll = Random.Shared.Next(1, 101);
+        bool hit = roll <= HitChance;
+
+    }
+
+    public class WeaponStrengthDamage
+    {
+        int Damage = (Character.Strength +/*weapon.damage*/)
+    }
+
+    public class dodge
+    {
+        int DodgeChance = Character.dexterity
+
+        if(HitChance.hit)
+            {
+       int Dodge = Random.Shared.Next(1, 101);
+        bool dodgedroll = Dodge <= DodgeChance
+            }
+
+       if(Dodge = DodgeChance)
+        {Console.WriteLine("attack was dodged");}
+        else
+{ Console.WriteLine("attack hit!")}
+
+
+    
+
+}
+}
