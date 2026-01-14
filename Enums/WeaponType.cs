a5c70117bab1c02854550bd16c4be2ce0004c3ec
21001506CTC <21001506@student.mukegonisd.org>
2026-01-14 10:21:40 -0500

    m

--- Enums/WeaponType.cs
+++ Enums/WeaponType.cs
@@ -106,39 +106,5 @@ namespace RPGFramework.Enums
         MainHand,
         OffHand
     }
-
-    internal enum WeaponMaterial
-    {
-        Wood,
-        rusty,
-        stone,
-        Iron,
-        Steel,
-        Mythril,
-        Obsidian
-    }
-
-    internal enum ArmorMaterial
-    {
-        Cloth,
-        Leather,
-        chainmail,
-        Iron,
-        Steel,
-        Mythril
-    }
-    internal enum ArmorSlot
-    {
-        Head,
-        Chest,
-        Legs,
-        back
-    }
-    internal enum ArmorType
-    {
-        Light,
-        Medium,
-        Heavy
-    }
 }
     
