7f985c52cc4a978f7fcbbc89765ecfa4e72ef214
Shelton Guthrie <shelman200@gmail.com>
2026-01-15 08:22:25 -0500

    sdgffgdfg

--- Character/NonPlayer.cs
+++ Character/NonPlayer.cs
@@ -11,43 +11,28 @@ namespace RPGFramework
     internal class NonPlayer : Character
     {
         //additional variables from NPCs
+        public int MaxAgressionLevel { get; private set; } = 10; // (the maximum aggression level for this NPC)
+        public int MinAgressionLevel { get; private set; } = 0; // (the minimum aggression level for this NPC)
 
 
         //Next two are paired together, the private in order to hold the actual value, the public to enforce clamping
         private int _currentAggressionLevel;
         public int CurrentAggressionLevel{
             get => _currentAggressionLevel;
-            protected set => _currentAggressionLevel = Math.Clamp(value, 0, 10);
+            private set => _currentAggressionLevel = Math.Clamp(value, 0, 10);
         }
         public Dictionary<string, string[]> DialogOptions { get; set; } //Dialog options, added at creation time.
         public int PreviousAreaId { get; private set; } = 0;
         public NonPlayer(string name, string Desc, int level, Dictionary<string, string[]> DialogOptions, int locationID)
         {
             Name = name;
-            Description = Desc;
             Level = level;
-            this.DialogOptions = DialogOptions;
-            LocationId = locationID;
         }
 
-        public NonPlayer()
-        {
-            Name ="Generic NPC";
-            Level = 1;
-            DialogOptions = new Dictionary<string, string[]>();
-            LocationId = 0;
-        }
-
-        //incriments the agression level by a set amount (negative values allowed)
         public void IncrimentAgressionLevel(int amount)
         {
             CurrentAggressionLevel += amount;
         }
-
-        //feels self explanitory
-        public int GetAggressionLevel(){return CurrentAggressionLevel;}
-
-        //returns the two different descriptions
         
     }
 }
