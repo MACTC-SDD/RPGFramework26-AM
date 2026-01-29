5de15919c9b45480ab30e69629694e971d407ee8
cbrownmactc <cbrown@muskegonisd.org>
2026-01-29 11:14:07 -0500

    Added log suppression timing.

--- Engine/GameState.cs
+++ Engine/GameState.cs
@@ -39,6 +39,7 @@ namespace RPGFramework
         private CancellationTokenSource? _timeOfDayCts;
         private Task? _timeOfDayTask;
 
+        private int _logSuppressionSeconds = 30;
         #endregion
 
         #region --- Properties ---
@@ -318,14 +319,25 @@ namespace RPGFramework
         #endregion --- Methods ---
 
         #region --- Static Methods ---
-        internal static void Log(DebugLevel level, string message)
+        internal static bool Log(DebugLevel level, string message)
         {
             if (level <= GameState.Instance.DebugLevel)
             {
                 Console.WriteLine($"[{level}] {message}");
+                return true;
             }
+            return false;
         }
 
+        internal static bool Log(DebugLevel level, string message, DateTime lastLog, int suppressionSeconds)
+        {
+            if ((DateTime.Now - lastLog).TotalSeconds >= suppressionSeconds)
+            {
+                return Log(level, message);
+            }
+
+            return false;
+        }
         #endregion
 
         #region --- Thread Methods ---
