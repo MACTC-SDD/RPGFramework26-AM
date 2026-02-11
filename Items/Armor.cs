2e226894b3a7b39dbd794320f2b000fecdc85967
21001506CTC <21001506@student.mukegonisd.org>
2026-02-11 10:14:14 -0500

    fix

--- Items/Armor.cs
+++ Items/Armor.cs
@@ -10,7 +10,7 @@ namespace RPGFramework
         public ArmorMaterial Material { get; set; }
         public ArmorSlot Slot { get; set; }
         public ArmorType Type { get; set; }
-        public int DamageReduction { get; set; }
+        public double DamageReduction { get; set; }
         public int Durability { get; set; }
         public int MaxDurability { get; set; }
         public float DodgeChance { get; set; }
